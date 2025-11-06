75875c0bca663d639b065caf0dfb489fcce785ca
isfbarrios <fabribarrios@hotmail.com>
2025-11-06 17:34:16 -0300

    Creacion del proyecto Java

---
+++ ProyectoFinal/src/test/java/com/ort/edu/proyectofinal/ProyectoFinal/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ort.edu.proyectofinal.ProyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
