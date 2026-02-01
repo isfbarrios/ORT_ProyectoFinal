e7eb93f1f96ab2055aadef9f4a1707a31a284b25
isfbarrios <fabribarrios@hotmail.com>
2026-01-31 23:26:05 -0300

    Se resuelve error. Queda funcionando el flujo nuevo, falta generar la factura

--- BasesDeDatos/Pruebas.sql
+++ BasesDeDatos/Pruebas.sql
@@ -95,138 +95,48 @@ ALTER TABLE `Order`
 ADD COLUMN `Rol` VARCHAR(30) NOT NULL AFTER `StateId`;
 
 
-
-select m.Name, c.*
-from CartItem c 
-join MenuItem m on (m.MenuItemId = c.MenuItemId)
-where c.CartId = 152;
-
-select * from CartState c;
-
-select * from `Order` o;
-
-select * from Sessions s;
-
-delete from Sessions where UserId = 0;
-
-select * from UserDirection u;
-
-drop table if exists OrderItem;
-
-INSERT INTO OrderItem (OrderId,CartId,ItemId,Quantity,ExtraData) VALUES
-	 (2,1,1,1,NULL),
-	 (3,3,3,1,NULL);
-
-Carrito
-
-CREATE TABLE `Order` (
-  KitchenOrderId INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  CartId INT UNSIGNED NOT NULL,
-  OrderDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  KitchenOrderStateId INT UNSIGNED NOT NULL,
-  Notes TEXT DEFAULT NULL,
-  LastUpdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (KitchenOrderId),
-  KEY ix_KitchenOrder_Cart (CartId),
-  KEY ix_KitchenOrder_State (KitchenOrderStateId),
-  CONSTRAINT fk_KitchenOrder_Cart
-    FOREIGN KEY (CartId) REFERENCES Cart (CartId)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-Bill
-
-CREATE TABLE Bill (
-  `BillId` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `BillNumber` varchar(30) NOT NULL,
-  `Amount` decimal(12,2) NOT NULL,
-  `Date` datetime NOT NULL DEFAULT current_timestamp(),
-  `PaymentTypeId` int(10) unsigned DEFAULT NULL,
-  `ExtraData` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`ExtraData`)),
-  PRIMARY KEY (`BillId`),
-  UNIQUE KEY `ux_Bill_BillNumber` (`BillNumber`),
-  KEY `fk_Bill_Payment` (`PaymentTypeId`),
-  KEY `ix_Bill_Date` (`Date`),
-  CONSTRAINT `fk_Bill_Payment` FOREIGN KEY (`PaymentTypeId`) REFERENCES `PaymentType` (`PaymentTypeId`),
-  CONSTRAINT `CONSTRAINT_1` CHECK (`Amount` >= 0)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
-
-CREATE TABLE `BillItem` (
-  `BillId` int(10) unsigned NOT NULL,
-  `CartId` int(10) unsigned NOT NULL,
-  `ItemId` int(10) unsigned NOT NULL,
-  `Quantity` int(10) unsigned NOT NULL,
-  `ExtraData` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`ExtraData`)),
-  PRIMARY KEY (`BillId`,`ItemId`),
-  KEY `fk_BillItem_CartItem` (`CartId`,`ItemId`),
-  CONSTRAINT `fk_BillItem_CartItem` FOREIGN KEY (`CartId`, `ItemId`) REFERENCES `CartItem` (`CartId`, `ItemId`),
-  CONSTRAINT `fk_BillItem_Bill` FOREIGN KEY (`BillId`) REFERENCES `Bill` (`BillId`) ON DELETE CASCADE,
-  CONSTRAINT `CONSTRAINT_1` CHECK (`Quantity` > 0)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-select * from OrderState;
-
-INSERT INTO OrderState (OrderStateId, Name) VALUES
-(1, 'Pendiente'),
-(2, 'Preparando'),
-(3, 'Listo'),
-(4, 'Completado'),
-(5, 'Cancelado');
-
-
-CREATE TABLE OrderState (
-  OrderStateId INT UNSIGNED NOT NULL,
-  Name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (OrderStateId),
-  UNIQUE KEY ux_OrderState_Name (Name)
-);
-
-
-
 CREATE TABLE OrderItem (
   OrderItemId INT UNSIGNED NOT NULL AUTO_INCREMENT,
   OrderId INT UNSIGNED NOT NULL,
-  ProductId INT UNSIGNED NOT NULL,
+  MenuItemId INT UNSIGNED NOT NULL,
   Quantity INT UNSIGNED NOT NULL,
   UnitPrice DECIMAL(12,2) NOT NULL,
   Notes TEXT DEFAULT NULL,
   PRIMARY KEY (OrderItemId),
   KEY ix_OrderItem_Order (OrderId),
   CONSTRAINT fk_OrderItem_Order
-    FOREIGN KEY (OrderId) REFERENCES Order (OrderId),
+    FOREIGN KEY (OrderId) REFERENCES `Order` (OrderId),
   CONSTRAINT chk_OrderItem_Qty CHECK (Quantity > 0),
   CONSTRAINT chk_OrderItem_Price CHECK (UnitPrice >= 0)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+drop table if exists OrderItem;
 
+ALTER TABLE OrderItem
+DROP  COLUMN ExtraData;
 
-select * from  `Order`;
+ALTER TABLE OrderItem
+ADD COLUMN Notes TEXT DEFAULT NULL,
 
-ALTER TABLE `Order`
-DROP FOREIGN KEY `fk_Order_State`;
+select * from `Order` o;
 
-ALTER TABLE `Order`
-DROP INDEX `fk_Order_State`;
+select * from OrderItem;
 
-ALTER TABLE `Order`
-DROP COLUMN `StateId`;
+select * from Cart c order by c.CartId desc;
 
-DROP TABLE OrderState;
-
-ALTER TABLE `Order`
-DROP FOREIGN KEY `fk_Order_Canal`;
-
-ALTER TABLE `Order`
-DROP INDEX `fk_Order_Canal`;
-
-
-ALTER TABLE `Order`
-DROP COLUMN `CanalId`;
-
-drop table if exists OrderInProgress;
+CREATE TABLE `Order` (
+  OrderId int(10) unsigned NOT NULL AUTO_INCREMENT,
+  CartId int(10) unsigned NOT NULL,
+  OrderDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  OrderStateId int(10) unsigned NOT NULL,
+  Notes TEXT DEFAULT NULL,
+  LastUpdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (OrderId),
+  KEY ix_Order_Cart (CartId),
+  KEY ix_Order_State (OrderStateId),
+  CONSTRAINT fk_Order_Cart
+    FOREIGN KEY (CartId) REFERENCES Cart (CartId)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 
